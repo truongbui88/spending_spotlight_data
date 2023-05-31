d804e6a3e5f7c47205a23f6ef5bfba5d0c8ad4b2
jordancampbell23 <jcampbel@callutheran.edu>
2023-05-31 14:51:33 -0400

    State Appendix updates

--- state-appendix.R
+++ state-appendix.R
@@ -1,6 +1,7 @@
 # Load libraries
 library(tidyverse)
 library(jsonlite)
+library(zoo)
 options(scipen = 999)
 
 # Load data
@@ -150,10 +151,85 @@ rev_data <- left_join(total_rev, fed_rev, by = c("Year", "State", "Format")) |>
 write_csv(rev_data, "output_data/appendix/rev_data_total.csv")
 
 # Write this data to json
-rev_data_total_json <- toJSON(rev_data, pretty = TRUE)
-write(rev_data_total_json, "output_data/appendix/rev_data_total.json")
+rev_data <- rev_data |>
+    as.data.frame() |>
+    filter(Format == "Per Student - Inflation Adjusted") |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+states <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming")
+
+# Federal Revenue
+federal_filter_and_map <- function(state) {
+    state_data <- rev_data %>%
+        filter(State == state)
+    map2(state_data$Year, state_data$`Federal Revenue`, function(x, y) {
+        list(x = unbox(as.character(x)), y = unbox(y))
+    })
+}
+
+# Apply the function to each state
+federal_revenue_indexed <- setNames(lapply(states, federal_filter_and_map), states)
+
+federal_revenue_indexed_json <- toJSON(federal_revenue_indexed, pretty = TRUE)
+write(federal_revenue_indexed_json, "output_data/appendix/federal_revenue_indexed.json")
+
+# State Revenue
+state_filter_and_map <- function(state) {
+    state_data <- rev_data %>%
+        filter(State == state)
+    map2(state_data$Year, state_data$`State Revenue`, function(x, y) {
+        list(x = unbox(as.character(x)), y = unbox(y))
+    })
+}
+
+# Apply the function to each state
+state_revenue_indexed <- setNames(lapply(states, state_filter_and_map), states)
+
+state_revenue_indexed_json <- toJSON(state_revenue_indexed, pretty = TRUE)
+write(state_revenue_indexed_json, "output_data/appendix/state_revenue_indexed.json")
+
+# Local Revenue
+local_filter_and_map <- function(state) {
+    state_data <- rev_data %>%
+        filter(State == state)
+    map2(state_data$Year, state_data$`Local Revenue`, function(x, y) {
+        list(x = unbox(as.character(x)), y = unbox(y))
+    })
+}
+
+# Apply the function to each state
+local_revenue_indexed <- setNames(lapply(states, local_filter_and_map), states)
+
+local_revenue_indexed_json <- toJSON(local_revenue_indexed, pretty = TRUE)
+write(local_revenue_indexed_json, "output_data/appendix/local_revenue_indexed.json")
 
 
+
+# Basic revenue data frame: state, local, and federal
+
+fed_rev_per_student <- fed_rev |>
+    filter(Format == "Per Student - Inflation Adjusted") |>
+    select(State, Year, `Federal Revenue`)
+
+state_rev_per_student <- state_rev |>
+    filter(Format == "Per Student - Inflation Adjusted") |>
+    select(State, Year, `State Revenue`)
+
+local_rev_per_student <- local_rev |>
+    filter(Format == "Per Student - Inflation Adjusted") |>
+    select(State, Year, `Local Revenue`)
+
+basic_rev_data <- fed_rev_per_student |>
+    left_join(state_rev_per_student, by = c("State", "Year")) |>
+    left_join(local_rev_per_student, by = c("State", "Year"))
+
+basic_rev_data <- basic_rev_data |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Save data as JSON
+basic_rev_data_json <- toJSON(basic_rev_data, pretty = TRUE)
+
+write(basic_rev_data_json, "output_data/appendix/basic_rev_data.json")
 
 
 # Create a table with `Total Revenue - Per Pupil` and Enrollment change for each state between 2002 and 2020
@@ -196,6 +272,10 @@ rev_enroll_change <- left_join(rev_change, enroll_change, by = "State")
 # Save data
 write_csv(rev_enroll_change, "output_data/appendix/rev_enrollment_table.csv")
 
+# Save data to JSON
+rev_enroll_change_json <- toJSON(rev_enroll_change, pretty = TRUE)
+
+write(rev_enroll_change_json, "output_data/appendix/rev_enrollment_table.json")
 
 # Salary & Benefits
 
@@ -256,6 +336,18 @@ ss_data_cpi_salary_benefits <- left_join(ss_data_cpi_salary, ss_data_cpi_benefit
 # Save data
 write_csv(ss_data_cpi_salary_benefits, "output_data/appendix/salary_benefits.csv")
 
+# Pivote ss_data_cpi_salary_benefits wide
+ss_data_cpi_salary_benefits_wide <- ss_data_cpi_salary_benefits |>
+    pivot_wider(
+        names_from = Format,
+        values_from = c(`Total Salary`, `Total Benefits`)
+    ) |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Write to JSON
+ss_data_cpi_salary_benefits_json <- toJSON(ss_data_cpi_salary_benefits_wide, pretty = TRUE)
+
+write(ss_data_cpi_salary_benefits_json, "output_data/appendix/salary_benefits.json")
 
 
 # Salary & Benefits Percent Change
@@ -283,3 +375,295 @@ ss_data_cpi_iii <- ss_data_cpi_iii |>
 
 
 write_csv(ss_data_cpi_iii, "output_data/appendix/salary_benefits_pct.csv")
+
+
+
+# Enrollment and Staffing Trends
+
+# Get enrollment data from ss_data_cpi
+enroll_data <- ss_data_cpi |>
+    select(Year, State, Enrollment)
+
+#  Get staffing data from 'input_data/teachers_non_teachers.csv'
+staff_data <- read_csv("input_data/teachers_non_teachers.csv") |>
+    select(Year, State, `All staff`, Teachers) |>
+    # create a column for non-teachers
+    mutate(`Non-Teachers` = `All staff` - Teachers) |>
+    select(Year, State, Teachers, `Non-Teachers`)
+
+
+# Combine enrollment and staffing data into one data frame
+enroll_staff_data <- left_join(enroll_data, staff_data, by = c("Year", "State"))
+
+# For each state calculate the percentage change from 2002 for enrollment, teachers, and non-teachers
+enroll_staff_data <- enroll_staff_data |>
+    mutate(
+        `Enrollment Percent Change` = Enrollment / Enrollment[Year == 2002] - 1,
+        `Teachers Percent Change` = Teachers / Teachers[Year == 2002] - 1,
+        `Non-Teachers Percent Change` = `Non-Teachers` / `Non-Teachers`[Year == 2002] - 1
+    ) |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Write the data to json
+enroll_staff_data_json <- toJSON(enroll_staff_data, pretty = TRUE)
+
+write(enroll_staff_data_json, "output_data/appendix/enroll_staff_data.json")
+
+
+# Teacher Salary Growth vs Revenue Per Student Growth
+
+# Get revenue data from ss_data_cpi
+rev_data <- ss_data_cpi |>
+    select(Year, State, `Total Revenue - Per Pupil`)
+
+# Get teacher salary data from input_data/salary_data.csv
+salary_data <- read_csv("input_data/salary_data.csv") |>
+    select(Year, State, `Salary Adj`)
+
+# Combine revenue and salary data into one data frame
+rev_salary_data <- left_join(rev_data, salary_data, by = c("Year", "State"))
+
+# Calculate the percentage change from 2002 for revenue and teacher salary
+rev_salary_data <- rev_salary_data |>
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2002] - 1,
+        `Salary Adj Percent Change` = `Salary Adj` / `Salary Adj`[Year == 2002] - 1
+    ) |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Write the data to json
+rev_salary_data_json <- toJSON(rev_salary_data, pretty = TRUE)
+
+write(rev_salary_data_json, "output_data/appendix/rev_salary_data.json")
+
+
+# NAEP
+
+# Load 4th and 8th grade NAEP data
+naep_math_8 <- read_csv("input_data/raw_naep/naep_8_math_ts.csv") |> arrange(Year, State)
+naep_math_4 <- read_csv("input_data/raw_naep/naep_4_math_ts.csv") |> arrange(Year, State)
+naep_reading_8 <- read_csv("input_data/raw_naep/naep_8_reading_ts.csv") |> arrange(Year, State)
+naep_reading_4 <- read_csv("input_data/raw_naep/naep_4_reading_ts.csv") |> arrange(Year, State)
+
+
+# Combine rev_data and naep data into one data frame
+
+# 8th grade math
+naep_math_8_rev <- naep_math_8 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(naep_math_8 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(naep_math_8 = na.approx(naep_math_8)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        naep_math_8_change = naep_math_8 / naep_math_8[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+# 4th grade math
+naep_math_4_rev <- naep_math_4 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(naep_math_4 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(naep_math_4 = na.approx(naep_math_4)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        naep_math_4_change = naep_math_4 / naep_math_4[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+# 8th grade reading
+naep_reading_8_rev <- naep_reading_8 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(naep_reading_8 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(naep_reading_8 = na.approx(naep_reading_8)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        naep_reading_8_change = naep_reading_8 / naep_reading_8[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+
+# 4th grade reading
+naep_reading_4_rev <- naep_reading_4 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(naep_reading_4 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(naep_reading_4 = na.approx(naep_reading_4)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        naep_reading_4_change = naep_reading_4 / naep_reading_4[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+
+# Pair down the variables
+naep_math_8_rev <- naep_math_8_rev |>
+    select(Year, State, `Total Revenue - Per Pupil`, `Total Revenue - Per Pupil Percent Change`, `naep_math_8`, `naep_math_8_change`)
+
+naep_math_4_rev <- naep_math_4_rev |>
+    select(Year, State, `naep_math_4`, `naep_math_4_change`)
+
+naep_reading_8_rev <- naep_reading_8_rev |>
+    select(Year, State, `naep_reading_8`, `naep_reading_8_change`)
+
+naep_reading_4_rev <- naep_reading_4_rev |>
+    select(Year, State, `naep_reading_4`, `naep_reading_4_change`)
+
+
+# Combine the data frames into one
+naep_rev_data <- naep_math_8_rev |>
+    left_join(naep_math_4_rev, by = c("Year", "State")) |>
+    left_join(naep_reading_8_rev, by = c("Year", "State")) |>
+    left_join(naep_reading_4_rev, by = c("Year", "State")) |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Write to json
+naep_rev_data_json <- toJSON(naep_rev_data, pretty = TRUE)
+
+write(naep_rev_data_json, "output_data/appendix/naep_rev_data.json")
+
+
+# Low Income NAEP
+
+# Load 4th and 8th grade NAEP data
+li_naep_math_8 <- read_csv("input_data/raw_naep/naep_8_math_low_income_ts.csv") |> arrange(Year, State)
+li_naep_math_4 <- read_csv("input_data/raw_naep/naep_4_math_low_income_ts.csv") |> arrange(Year, State)
+li_naep_reading_8 <- read_csv("input_data/raw_naep/naep_8_reading_low_income_ts.csv") |> arrange(Year, State)
+li_naep_reading_4 <- read_csv("input_data/raw_naep/naep_4_reading_low_income_ts.csv") |> arrange(Year, State)
+
+
+# Combine rev_data and naep data into one data frame
+
+# 8th grade math
+li_naep_math_8_rev <- li_naep_math_8 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(li_naep_math_8 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(li_naep_math_8 = na.approx(li_naep_math_8)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        li_naep_math_8_change = li_naep_math_8 / li_naep_math_8[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+# 4th grade math
+li_naep_math_4_rev <- li_naep_math_4 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(li_naep_math_4 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(li_naep_math_4 = na.approx(li_naep_math_4)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        li_naep_math_4_change = li_naep_math_4 / li_naep_math_4[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+# 8th grade reading
+li_naep_reading_8_rev <- li_naep_reading_8 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(li_naep_reading_8 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(li_naep_reading_8 = na.approx(li_naep_reading_8)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        li_naep_reading_8_change = li_naep_reading_8 / li_naep_reading_8[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+
+# 4th grade reading
+li_naep_reading_4_rev <- li_naep_reading_4 |>
+    filter(State != "District of Columbia") |>
+    filter(State != "DoDEA") |>
+    filter(State != "Puerto Rico") |>
+    filter(Year != 2022) |>
+    right_join(rev_data, by = c("Year", "State")) |>
+    filter(Year != 2002) |>
+    filter(Year != 2020) |>
+    arrange(State, Year) |>
+    rename(li_naep_reading_4 = NAEP) |>
+    group_by(State) |> # You need to group by state as each state will have its own timeline
+    mutate(li_naep_reading_4 = na.approx(li_naep_reading_4)) |> # This does the linear interpolation
+    mutate(
+        `Total Revenue - Per Pupil Percent Change` = `Total Revenue - Per Pupil` / `Total Revenue - Per Pupil`[Year == 2003] - 1,
+        li_naep_reading_4_change = li_naep_reading_4 / li_naep_reading_4[Year == 2003] - 1
+    ) |>
+    ungroup()
+
+
+# Pair down the variables
+li_naep_math_8_rev <- li_naep_math_8_rev |>
+    select(Year, State, `Total Revenue - Per Pupil`, `Total Revenue - Per Pupil Percent Change`, `li_naep_math_8`, `li_naep_math_8_change`)
+
+li_naep_math_4_rev <- li_naep_math_4_rev |>
+    select(Year, State, `li_naep_math_4`, `li_naep_math_4_change`)
+
+li_naep_reading_8_rev <- li_naep_reading_8_rev |>
+    select(Year, State, `li_naep_reading_8`, `li_naep_reading_8_change`)
+
+li_naep_reading_4_rev <- li_naep_reading_4_rev |>
+    select(Year, State, `li_naep_reading_4`, `li_naep_reading_4_change`)
+
+
+# Combine the data frames into one
+li_naep_rev_data <- li_naep_math_8_rev |>
+    left_join(li_naep_math_4_rev, by = c("Year", "State")) |>
+    left_join(li_naep_reading_8_rev, by = c("Year", "State")) |>
+    left_join(li_naep_reading_4_rev, by = c("Year", "State")) |>
+    mutate(Year = paste0(Year, "-01-01"))
+
+# Write to json
+li_naep_rev_data_json <- toJSON(li_naep_rev_data, pretty = TRUE)
+
+write(li_naep_rev_data_json, "output_data/appendix/li_naep_rev_data.json")
